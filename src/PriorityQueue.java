2b74f0c2ba49372ddf52a5cb8071a8e69df037be
e <a1924377643@163.com>
2018-01-27 23:02:00 +0800

    Priority Queue

---
+++ src/PriorityQueue.java
@@ -0,0 +1,32 @@
+public class PriorityQueue<Key extends Comparable<Key>> {
+    public PriorityQueue(){}
+
+    public PriorityQueue(int max){
+
+    }
+
+    public PriorityQueue(Key[] a){
+
+    }
+
+    public Key deleteMax(){
+        return null;
+    }
+
+    public void insert(Key a){
+
+    }
+
+    public Key max(){
+        return null;
+    }
+
+    public boolean isEmpty(){
+        return false;
+    }
+
+    public int size(){
+        return 0;
+    }
+
+}
