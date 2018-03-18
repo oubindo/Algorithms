8bce289f84744619040bb0e5de83d08bc80e8912
oubindo <a1924377643@163.com>
2018-03-18 21:05:07 +0800

    最终版本，优化了Outcast

--- src/wordnet/SAP.java
+++ src/wordnet/SAP.java
@@ -7,97 +7,86 @@ import edu.princeton.cs.algs4.StdOut;
 
 
 public class SAP {
-    private Digraph mGraph;
-    private AncestorBFS mBfs;
+    private Digraph G;
 
-    // constructor takes a digraph (not necessarily a DAG)
+    // constructor takes a digraph (not necessarily a DAG)  
     public SAP(Digraph G){
-        if (G == null){
-            throw new IllegalArgumentException("Argument is Null");
-        }
-        mGraph = G;
+        this.G = new Digraph(G);
     }
 
-    private int[] shortest(int v, int w){
-        validate(v, w);
-        int[] result = new int[2];
-        AncestorBFS vBfs = new AncestorBFS(mGraph, v);
-        AncestorBFS wBfs = new AncestorBFS(mGraph, v);
-        boolean[] vMask = vBfs.getAnestor();
-        boolean[] wMask = wBfs.getAnestor();
-        int shortLength = Integer.MAX_VALUE;
-        int shortAncestor = Integer.MAX_VALUE;
-        for (int i = 0; i < vMask.length; i++){
-            if (vMask[i] && wMask[i]){
-                int temp = vBfs.distTo(i) + wBfs.distTo(i);
-                if (shortLength > temp){
-                    shortLength = temp;
-                    shortAncestor = i;
-                }
-            }
-        }
-        if (shortLength == Integer.MAX_VALUE){
-            result[0] = -1;
-            result[1] = -1;
-            return result;
-        }
-        result[0] = shortLength;
-        result[1] = shortAncestor;
-        return result;
-    }
-
-    private int[] shortest(Iterable<Integer> v, Iterable<Integer> w){
-        int[] result = new int[2];
-        int shortLength = Integer.MAX_VALUE;
-        int shortAncestor = Integer.MAX_VALUE;
-        for (int vs : v){
-            for (int ws : w){
-                int[] tempResult = shortest(vs, ws);
-                if (tempResult[0] != -1 && tempResult[0] < shortLength){
-                    shortLength = tempResult[0];
-                    shortAncestor = tempResult[1];
-                }
-            }
-        }
-        if (shortLength == Integer.MAX_VALUE){
-            result[0] = -1;
-            result[1] = -1;
-            return result;
-        }
-        result[0] = shortLength;
-        result[1] = shortAncestor;
-        return result;
-    }
-
-
-    // length of shortest ancestral path between v and w; -1 if no such path
+    // length of shortest ancestral path between v and w; -1 if no such path  
     public int length(int v, int w){
         int[] result = shortest(v, w);
         return result[0];
     }
 
-    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path
+    // a common ancestor of v and w that participates in a shortest ancestral path; -1 if no such path  
     public int ancestor(int v, int w){
         int[] result = shortest(v, w);
         return result[1];
     }
 
-    private void validate(int v, int w) {
-        if (v < 0 || v >= mGraph.V() || w < 0 || w >= mGraph.V()){
-            throw new IndexOutOfBoundsException();
-        }
-    }
-
-    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path
+    // length of shortest ancestral path between any vertex in v and any vertex in w; -1 if no such path  
     public int length(Iterable<Integer> v, Iterable<Integer> w){
         int[] result = shortest(v, w);
         return result[0];
     }
 
-    // a common ancestor that participates in shortest ancestral path; -1 if no such path
+    // a common ancestor that participates in shortest ancestral path; -1 if no such path  
     public int ancestor(Iterable<Integer> v, Iterable<Integer> w){
         int[] result = shortest(v, w);
         return result[1];
+    }
+
+    private int[] shortest(int v, int w){
+        int[] result = new int[2];
+        AncestorBFS vBFS = new AncestorBFS(G, v);
+        AncestorBFS wBFS = new AncestorBFS(G, w);
+        boolean[] vMarked = vBFS.getAnestor();
+        boolean[] wMarked = wBFS.getAnestor();
+        int shortestLength = Integer.MAX_VALUE;
+        int tempLength = Integer.MAX_VALUE;
+        int shortestAncestor = Integer.MAX_VALUE;
+        for (int i=0; i<vMarked.length; i++){
+            if (vMarked[i] && wMarked[i]){
+                tempLength = vBFS.distTo(i) + wBFS.distTo(i);
+                if (tempLength < shortestLength){
+                    shortestLength = tempLength;
+                    shortestAncestor = i;
+                }
+            }
+        }
+        if (shortestLength == Integer.MAX_VALUE){
+            result[0] = -1;
+            result[1] = -1;
+            return result;
+        }
+        result[0] = shortestLength;
+        result[1] = shortestAncestor;
+        return result;
+    }
+
+    private int[] shortest(Iterable<Integer> v, Iterable<Integer> w){
+        int shortestAncestor = Integer.MAX_VALUE;
+        int shortestLength = Integer.MAX_VALUE;
+        int[] result = new int[2];
+        for (int vNode : v){
+            for (int wNode : w){
+                int[] tempResult = shortest(vNode, wNode);
+                if (tempResult[0] != -1 && tempResult[0] < shortestLength){
+                    shortestLength = tempResult[0];
+                    shortestAncestor = tempResult[1];
+                }
+            }
+        }
+        if (shortestLength == Integer.MAX_VALUE){
+            result[0] = -1;
+            result[1] = -1;
+            return result;
+        }
+        result[0] = shortestLength;
+        result[1] = shortestAncestor;
+        return result;
     }
 
     // do unit testing of this class
--- src/wordnet/WordNet.java
+++ src/wordnet/WordNet.java
@@ -9,8 +9,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.LinkedList;
-import java.util.Iterator;
-import java.util.Stack;
 
 public class WordNet {
 
